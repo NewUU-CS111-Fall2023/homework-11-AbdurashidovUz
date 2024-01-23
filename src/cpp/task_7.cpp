f42269ad12c8b02492b36d0efb6053cb767e5c99
AbdurashidovUz <127374337+AbdurashidovUz@users.noreply.github.com>
2024-01-23 23:30:23 +0500

    Update task_7.cpp

--- src/cpp/task_7.cpp
+++ src/cpp/task_7.cpp
@@ -1,9 +1,22 @@
-/*
- * Author:
- * Date:
- * Name:
- */
+#include <iostream>
 
-class Problem7 {
-public:
-};
+int eulerTotientFunction(int a, int b) {
+    return (a - 1) * (b - 1);
+}
+
+int main() {
+    int a, b;
+
+    std::cout << "Enter the first prime number (a): ";
+    std::cin >> a;
+
+    std::cout << "Enter the second prime number (b): ";
+    std::cin >> b;
+
+
+    int result = eulerTotientFunction(a, b);
+
+    std::cout << "Euler's totient function for n = " << a * b << " is: " << result << std::endl;
+
+    return 0;
+}
