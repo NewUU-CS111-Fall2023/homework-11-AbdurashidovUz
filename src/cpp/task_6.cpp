ab1640b6bfeb7cf6926da9e0e06e39dcc91d48fa
AbdurashidovUz <127374337+AbdurashidovUz@users.noreply.github.com>
2024-01-23 23:30:07 +0500

    Update task_6.cpp

--- src/cpp/task_6.cpp
+++ src/cpp/task_6.cpp
@@ -1,9 +1,46 @@
-/*
- * Author:
- * Date:
- * Name:
- */
+#include <iostream>
+#include <cmath>
 
-class Problem6 {
-public:
-};
+bool isPrime(int num) {
+    if (num <= 1) {
+        return false;
+    }
+
+    int sqrtNum = sqrt(num);
+    for (int i = 2; i <= sqrtNum; ++i) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void generatePrimesGreaterThanN(int n, int& prime1, int& prime2) {
+    prime1 = n + 1;
+
+    while (true) {
+        if (isPrime(prime1)) {
+            prime2 = prime1 + 1;
+            while (!isPrime(prime2)) {
+                ++prime2;
+            }
+
+            return;
+        }
+        ++prime1;
+    }
+}
+
+int main() {
+    int n;
+    std::cout << "Enter a number (n): ";
+    std::cin >> n;
+
+    int prime1, prime2;
+    generatePrimesGreaterThanN(n, prime1, prime2);
+
+    std::cout << "Two prime numbers greater than " << n << " are: " << prime1 << " and " << prime2 << std::endl;
+
+    return 0;
+}
