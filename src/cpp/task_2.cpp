0a0edb9a7db73589c5cc52c34f1cef093b34744e
AbdurashidovUz <127374337+AbdurashidovUz@users.noreply.github.com>
2024-01-23 23:28:15 +0500

    Update task_2.cpp

--- src/cpp/task_2.cpp
+++ src/cpp/task_2.cpp
@@ -1,9 +1,30 @@
-/*
- * Author:
- * Date:
- * Name:
- */
+#include <vector>
 
 class Problem2 {
 public:
+    bool canPartition(std::vector<int>& nums) {
+        int total_sum = 0;
+        for (int num : nums) {
+            total_sum += num;
+        }
+
+        if (total_sum % 2 != 0) {
+            return false;
+        }
+
+        int subset_sum = total_sum / 2;
+        std::vector<bool> dp(subset_sum + 1, false);
+        dp[0] = true;
+
+        for (int num : nums) {
+            for (int _sum = subset_sum; _sum >= num; --_sum) {
+                dp[_sum] = dp[_sum] || dp[_sum - num];
+                if (dp[subset_sum]) {
+                    return dp[subset_sum];
+                }
+            }
+        }
+
+        return dp[subset_sum];
+    }
 };
