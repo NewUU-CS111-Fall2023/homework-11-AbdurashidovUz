9f56239d8bc22a9c648ec29923f0d4482bbab089
AbdurashidovUz <127374337+AbdurashidovUz@users.noreply.github.com>
2024-01-23 23:29:29 +0500

    Update task_4.cpp

--- src/cpp/task_4.cpp
+++ src/cpp/task_4.cpp
@@ -1,8 +1,24 @@
-/*
- * Author:
- * Date:
- * Name:
- */
-
 class Problem4 {
+private:
+    static const int MOD = 1337;
+
+    int powMod(int a, int b) {
+        if (b == 0) return 1;
+        a = a % MOD;
+        int res = a;
+        for (int i = 1; i < b; i++)
+            res = (res * a) % MOD;
+        return res;
+    }
+
+public:
+    int superPow(int a, vector<int>& b) {
+        a = a % MOD;
+        if (a == 0) return a;
+        if (b.empty()) return 1;
+        int res = powMod(a, b[0]);
+        for (int i = 1; i < b.size(); i++)
+            res = (powMod(res, 10) * powMod(a, b[i])) % MOD;
+        return res;
+    }
 };
